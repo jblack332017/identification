c2c13143d4645e53a70776bcbd0de4977a055945
Josh <josh.m.black@hotmail.com>
2017-02-06 17:03:08 -0700

    add

--- blast.sh
+++ blast.sh
@@ -3,7 +3,7 @@
 time /fslhome/jblack33/fsl_groups/fslg_RidgeLab/compute/software/blast/ncbi-blast-2.2.31+/bin/blastn \
 	-query inputFastas/$1 \
 	-db blastdb/firstDatabase \
-	-outfmt 5 \
+	-outfmt 5\
 	-out blastOutput/$1 
 
 exit $?
